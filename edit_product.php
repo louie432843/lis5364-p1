e1bee977d41334c653a0fe1cbbffa1520ead95ea
timkanke <timkanke@gmail.com>
2013-03-07 21:33:32 -0500

    Update edit_product.php

--- edit_product.php
+++ edit_product.php
@@ -14,10 +14,11 @@ if (empty($code) || empty($name) || empty($price) ) {
     // If valid, edit the product
     require_once('global/database.php');
     $query = "UPDATE products
-    		  WHERE productID = '$product_id'
-                 (categoryID, productCode, productName, listPrice)
-              VALUES
-                 ('$category_id', '$code', '$name', '$price')";
+          SET categoryID = '$category_id',
+              productCode = '$code',
+              productName = '$name',
+              listPrice = '$price'
+          WHERE productID = $product_id";
                  
     $db->exec($query);
 
